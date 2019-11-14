a9c939ea8705f50d387aeb3a82653325267aed6c
21daniels <35512208+21daniels@users.noreply.github.com>
2019-11-14 14:32:28 -0600

    Add files via upload

--- A-Hall Sock Exchange/src/sockExchange.java
+++ A-Hall Sock Exchange/src/sockExchange.java
@@ -1,17 +1,39 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import static java.lang.System.*;
 
 public class sockExchange
 {
-    public static void main(String[] args) implements IOException
-    {
-        Scanner f = new Scanner(new File("src/students.dat"));
+    public static void main(String[] args) throws IOException {
+        int numOfStudents = 80;
 
-        while(f.hasNextLine())
-        {
-            out.println(f.nextLine());
+        PrintWriter writer = new PrintWriter("names.txt", "UTF-8");
+
+        for (int x = 0; x < 2; x++) {
+            Scanner f = new Scanner(new File("students.dat"));
+            Integer[] arr = new Integer[numOfStudents];
+            String[] names = new String[numOfStudents];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i;
+            }
+            Collections.shuffle(Arrays.asList(arr));
+
+            for (int i = 0; i < numOfStudents; i++) {
+                String studentName = f.nextLine();
+                String studentNumber = arr[i].toString();
+                names[arr[i]] = studentName;
+            }
+
+            for (int i = 0; i < numOfStudents; i++) {
+
+                writer.println(names[i] + "  -  " + names[++i]);
+            }
+            writer.close();
         }
     }
 }
